3e252a958de44f40a5f688edbaa4e0faf16c31fe
brunoCerqueiraa <profissional.brunocerqueira@hotmail.com>
2020-02-10 13:15:00 -0200

    Cor em possíveis movimentos

--- src/application/UI.java
+++ src/application/UI.java
@@ -60,18 +60,36 @@ public class UI {
 			System.out.print((8 - i) + " ");
 			for (int j = 0; j < pieces.length; j++)
 			{
-				printPiece(pieces[i][j]);
+				printPiece(pieces[i][j], false);
+			}
+			System.out.println();
+		}
+		System.out.println("  a b c d e f g h");
+	}
+	
+	public static void printBoard(ChessPiece[][] pieces, boolean[][] posssibleMoves)
+	{
+		for (int i = 0; i < pieces.length; i++)
+		{
+			System.out.print((8 - i) + " ");
+			for (int j = 0; j < pieces.length; j++)
+			{
+				printPiece(pieces[i][j], posssibleMoves[i][j]);
 			}
 			System.out.println();
 		}
 		System.out.println("  a b c d e f g h");
 	}
 
-	private static void printPiece(ChessPiece piece)
+	private static void printPiece(ChessPiece piece, boolean background)
 	{
+		if(background)
+		{
+			System.out.print(ANSI_BLUE_BACKGROUND);
+		}
 		if (piece == null)
 		{
-            System.out.print("-");
+            System.out.print("-" + ANSI_RESET);
         }
         else
         {
@@ -86,4 +104,6 @@ public class UI {
         }
         System.out.print(" ");
 	}
+	
+	
 }
